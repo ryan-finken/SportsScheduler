9688edf65b25040eb0b25bfd86de268b9e1cd90c
Ryan Finken <ryanfinken338@gmail.com>
2021-10-06 12:37:10 -0500

    fixed password issue

--- Play4All/src/main/java/org/launchcode/Play4All/models/dto/RegisterFormDTO.java
+++ Play4All/src/main/java/org/launchcode/Play4All/models/dto/RegisterFormDTO.java
@@ -1,8 +1,23 @@
 package org.launchcode.Play4All.models.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
 public class RegisterFormDTO extends LoginFormDTO{
 
+
+    //@Email
+    //private String email;
+
     private String verifyPassword;
+
+    //public String getEmail() {
+    //    return email;
+   // }
+
+   // public void setEmail(String email) {
+    //    this.email = email;
+   // }
 
     public String getVerifyPassword(){
         return verifyPassword;
